fab7570d5ba3c2a3eac5806b16bc3dac48d6872d
Durgalakshmi.Kumar <durgalakshmi.kumar@verramobility.com>
2025-06-03 23:53:24 +0530

    base

---
+++ authapp/src/test/java/com/sample/task/authapp/AuthappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.task.authapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
